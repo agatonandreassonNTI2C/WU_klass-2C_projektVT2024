f4ee80b631ce7f9cc13261c9612c24ca62661499
agatonandreassonNTI2C <150041085+agatonandreassonNTI2C@users.noreply.github.com>
2024-03-06 13:18:44 +0100

    Flashcards och text sidorna

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const svar = document.getElementById('svar')
 const flashcards_article = document.getElementById('flashcards_article')
 
+
+
 function toggleMenu() {
 
     svar.classList.toggle('visa_svar')
-
 }
 
 
